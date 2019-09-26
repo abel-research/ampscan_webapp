6052f64248511b2339899094a254a1ee3ca80461
jack-parsons <jack.parsons.uk@icloud.com>
2019-09-26 13:03:49 +0100

    Centering white on zero

--- test_app/static/test_app/js/process_scans.js
+++ test_app/static/test_app/js/process_scans.js
@@ -4,6 +4,10 @@ function createLUT() {
     const lookupTable = window.vtkNewLookupTable.newInstance();
     const numColors = getNumberOfColours();
     lookupTable.setNumberOfColors(numColors);
+    let smn = document.getElementById("scalarMax").value;
+    let smx = document.getElementById("scalarMin").value;
+    let zero_proportion = smx / (smx-smn);
+
     if (isAbsErrorEnabled()) {
         lookupTable.setColors([
              [212, 221, 225, 0],
@@ -12,7 +16,7 @@ function createLUT() {
     } else {
         lookupTable.setColors([
              [37.0, 48.0, 94.0, 0.0],
-             [212.0, 221.0, 225.0, 0.5],
+             [212.0, 221.0, 225.0, zero_proportion],
              [170.0, 75.0, 65.0, 1.0]
             ]);
     }
