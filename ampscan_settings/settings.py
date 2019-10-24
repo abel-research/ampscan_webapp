57c9064aaf9a97cebaba92f93b7df3d17099a65d
Zak Marine <marinezak@gmail.com>
2019-10-24 15:09:40 +0100

    fixed os path issues from windows vs linux

--- ampscan_settings/settings.py
+++ ampscan_settings/settings.py
@@ -14,6 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+print(BASE_DIR)
 
 
 # Quick-start development settings - unsuitable for production
@@ -140,7 +141,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'assets'),
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR+"/staticfiles/")
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 STATIC_URL = '/staticfiles/'
 
