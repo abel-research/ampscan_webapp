11b4aa96f9786dcdf0a6199a85b0821480e3b101
Joshua Steer <Joshua.Steer@soton.ac.uk>
2020-07-29 15:08:11 +0100

    Update settings.py

--- ampscan_settings/settings.py
+++ ampscan_settings/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 
 def generate_secret_key(file_name):
