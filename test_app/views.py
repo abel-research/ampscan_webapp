1eac0e91d35bcfefaf2c0dcd6aa6c53691e21909
Zak <zak@radiidevices.com>
2020-09-24 18:11:12 +0100

    removed exception

--- test_app/views.py
+++ test_app/views.py
@@ -455,7 +455,7 @@ def upload_view(request):
                 # "values": obj.values.flatten().tolist()
             }
             outViews[view]["amp_obj"] = amp_obj
-            raise Exception(outViews)
+            # raise Exception(outViews)
         request.session["obj_views"] = outViews
         # raise Exception(get_session(request).get_obj_views())
 
