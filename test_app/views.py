a6d30ab704b8ad2ce728e46afb8f2bd266b268d7
JoshuaSteer <Joshua.Steer@soton.ac.uk>
2020-09-25 10:16:29 +0100

    Tidied up commit

--- test_app/views.py
+++ test_app/views.py
@@ -166,6 +166,7 @@ def register_view(request):
     outViews = {}
     for view in views:
         try:
+            obj = ampEnv.get_object_view(view).ampObject
             outViews[view] = views[view].property_response()
             amp_obj = {
                 "vert": list(obj.vert.flatten().tolist()),
@@ -197,6 +198,7 @@ def register_export_view(request):
     outViews = {}
     for view in views:
         try:
+            obj = ampEnv.get_object_view(view).ampObject
             outViews[view] = views[view].property_response()
             amp_obj = {
                 "vert": list(obj.vert.flatten().tolist()),
